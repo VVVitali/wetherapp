eabec928c605bad6551865844588c500934088e3
user <brezent12345@gmail.com>
2022-07-21 01:28:30 +0300

    b

--- weatherApp/pril/views.py
+++ weatherApp/pril/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+import requests
+
 
 def index(request):
-    return render(request,('pril/index.html'))
+    appid = '92eb8a03f4408b2134d0259be100c9ae'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
+    city = 'Grodno'
+    res = requests.get(url.format(city))
+    print(res.text)
+    return render(request, ('pril/index.html'))
+
 
 def second(request):
-    return render(request,('pril/second.html'))
+    return render(request, ('pril/second.html'))
