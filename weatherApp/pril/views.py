9d7845c3a5de3798d3919535e936c56586099bfc
user <brezent12345@gmail.com>
2022-07-27 13:03:58 +0300

    g

--- weatherApp/pril/views.py
+++ weatherApp/pril/views.py
@@ -5,11 +5,30 @@ import requests
 def index(request):
     appid = '92eb8a03f4408b2134d0259be100c9ae'
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
-    city = 'Grodno'
-    res = requests.get(url.format(city))
-    print(res.text)
-    return render(request, ('pril/index.html'))
+    city = 'Гродно'
+    res = requests.get(url.format(city)).json()
+    city_info = {
+        'city': city,
+        'temp': res['main']['temp'],
+        'icon': res['weather'][0]['icon'],
+        'speed': res['wind']['speed']
+    }
+
+    context = {'info': city_info}
+    return render(request, ('pril/index.html'),context)
 
 
 def second(request):
-    return render(request, ('pril/second.html'))
+    appid = '92eb8a03f4408b2134d0259be100c9ae'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
+    city = 'Минск'
+    res = requests.get(url.format(city)).json()
+    city_info = {
+        'city': city,
+        'temp': res['main']['temp'],
+        'icon': res['weather'][0]['icon'],
+        'speed': res['wind']['speed']
+    }
+
+    context = {'info': city_info}
+    return render(request, ('pril/second.html'), context)
